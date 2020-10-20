2d3955a79548af3c2b0f3264242318a3996ed067
Natjoe <57070788+Natjoe@users.noreply.github.com>
2020-10-19 19:27:57 -0500

    Add files via upload

---
+++ VTI Final Proj/Pop_movies - Copy.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+   
+    <link rel="stylesheet" href="css\style.css">
+    <title>Popular Movies</title>
+</head>
+<body>
+  <div class="container_series">
+      <h1>Choose Movie</h1> 
+<div class="Row">
+    <div class="img-container"><img src="css\img\Movie_posters\Bad_Boys_for_Life_poster.jpg" onclick ="location.href='Movie_1_info.php'"><a href="Movie_1_info.php">img file</a></div>
+    <div class="img-container"><img src="css\img\Movie_posters\Dolittle_(2020_film_poster).png" onclick ="location.href='Movie_2_info.php'"><a href="Movie_2_info.php">img file</a></div>
+    <div class="img-container"><img src="css\img\Movie_posters\Mulan_(2020_film)_poster.jpg" onclick ="location.href='Movie_3_info.php'"><a href="Movie_3_info.php">img file</a></div>
+    <div class="img-container"><img src="css\img\Movie_posters\Scooby.jpg" onclick ="location.href='Movie_4_info.php'"><a href="Movie_4_info.php">img file</a></div>
+    <div class="img-container"><img src="css\img\Movie_posters\Sonic_the_Hedgehog_poster.jpg" onclick ="location.href='Movie_5_info.php'"><a href="Movie_5_info.php">img file</a></div>
+    <div class="img-container"><img src="css\img\Movie_posters\bloodshot.jpg" onclick ="location.href='Movie_6_info.php'"><a href="Movie_6_info.php">img file</a></div>
+</div>
+
+<div class="Row">
+    <div class="img-container"><img src="css\img\Movie_posters\Joker_Poster.jpg" onclick ="location.href='Movie_7_info.php'"><a href="Joker.html">img file</a></div>
+    <div class="img-container"><img src="css\img\Movie_posters\Escape_from_Pretoria_film_poster.png" onclick ="location.href='Movie_8_info.php'"><a href="Pretoria.html">img file</a></div>
+    <div class="img-container"><img src="css\img\Movie_posters\Tenet_movie_postertest.jpg" onclick ="location.href='Movie_9_info.php'"><a href="Tenet.html">img file</a></div>
+    <div class="img-container"><img src="css\img\Movie_posters\Terminator_Dark_Fate-Postertest.jpg" onclick ="location.href='Movie_10_info.php'"><a href="Terminator.html">img file</a></div>  
+    <div class="img-container"><img src="css\img\Movie_posters\grudge_movie_poster.jpg" onclick ="location.href='Movie_11_info.php'"><a href="The Grudge.html">img file</a></div>
+    <div class="img-container"><img src="css\img\Movie_posters\birds-of-prey_poster.jpg" onclick ="location.href='Movie_12_info.php'"><a href="Birds of Prey.html">img file</a></div>
+</div>
+
+<div class="Row">
+<div class="img-container"><img src="css\img\Movie_posters\The_Lodge_poster.jpg" onclick ="location.href='Movie_13_info.php'"><a href="The Lodge.html">img file</a></div>
+<div class="img-container"><img src="css\img\Movie_posters\Vanguard_poster.jpg" onclick ="location.href='Movie_14_info.php'"><a href="Vanguard.html">img file</a></div>
+<div class="img-container"><img src="css\img\Movie_posters\black_widow_2020_poster.jpg" onclick ="location.href='Movie_15_info.php'"><a href="Black Widow.html">img file</a></div>
+<div class="img-container"><img src="css\img\Movie_posters\Malang Poster.jpg" onclick ="location.href='Movie_16_info.php'"><a href="Malang.html">img file</a></div>
+<div class="img-container"><img src="css\img\Movie_posters\Archive_poster.jpg" onclick ="location.href='Movie_17_info.php'"><a href="Archive.html">img file</a></div>
+<div class="img-container"><img src="css\img\Movie_posters\Jungle_Cruise_poster.jpg" onclick ="location.href='Movie_18_info.php'"><a href="Jungle Cruise.html">img file</a></div>
+</div>
+</body>
+</html>
